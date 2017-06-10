35fd5054c625d7360fc7a3ac272b00bb989f6334
Renato Neto <rnato.netoo@gmail.com>
2017-06-10 15:03:47 -0300

    basic routes pages

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+        
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão é visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+);
