48bf115305a8a298013068c038b58c313adc2b97
Renato Neto <rnato.netoo@gmail.com>
2017-06-10 17:35:55 -0300

    some frontend components and functions

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,14 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
 export default class Todo extends Component {
-    render(){
+    constructor(props) {
+        super(props);
+        this.state = { description: '', list: [] }
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleAdd() {
+        console.log(this.state.description);
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value});
+    }
+
+    render() {
         return (
             <div>
                 <PageHeader name='Tasks' small='Registry' />
-                <TodoForm />
+                <TodoForm
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}
+                    description={this.state.description} />
                 <TodoList />
             </div>
         );
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import Grid from '../template/grid';
+import IconButtom from '../template/iconButton';
 
 export default props => (
     <div role='form' className='todoForm'>
-        <div className='col-xs-12 col-sm-9 col-md-10'>
-            <input id='description' className='form-control' placeholder='Add a task'></input>
-        </div>
-        <div className='col-xs-12 col-sm-3 col-md-2'>
-            <button className='btn btn-primary'>
-                <i className='fa fa-plus'></i>
-            </button>
-        </div>
+        <Grid cols='12 9 10'>
+            <input
+                id='description'
+                className='form-control'
+                placeholder='Add a task'
+                value={props.description}
+                onChange={props.handleChange}></input>
+        </Grid>
+        <Grid cols='12 3 2'>
+            <IconButtom
+                style='primary'
+                icon='plus'
+                onClick={props.handleAdd}></IconButtom>
+        </Grid>
     </div>
 );
